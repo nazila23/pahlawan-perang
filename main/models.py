e68d5925c601725a6d72c9d325ca9b2f197c92bf
nazila23 <nazila.pudjakesuma@gmail.com>
2021-11-24 09:11:53 +0700

    fix index,login,regis di django

--- main/models.py
+++ main/models.py
@@ -15,4 +15,20 @@ class anggota(models.Model):
     no_tlpn =models.IntegerField()
     fax =models.IntegerField()
     Email =models.TextField(max_length=200)
+
+class buku(models.Model):
+    judul = models.TextField(max_length=200)
+    pengarang = models.TextField(max_length=200)
+    edisi = models.CharField(max_length=200)
+    terbit = models.TextField(max_length=200)
+    tempat_terbit = models.TextField(max_length=200)
+    Deskripsi_fisik = models.TextField(max_length=200)
+    judul_seri = models.TextField(max_length=200)
+    klasifikasi = models.TextField(max_length=200)
+    No_panggil = models.TextField(max_length=200)
+    Subyek = models.TextField(max_length=200)
+    bahasa = models.TextField(max_length=200)
+    gambar_sampul = models.TextField(max_length=200)
+    
+
     
--- main/views.py
+++ main/views.py
@@ -1,14 +1,10 @@
 from django.core.checks import messages
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http.response import Http404
-# from django.shortcuts import redirect, render
-# from.import models
 
 
+def index(request):
+    return render(request,'index.html')
 
-# def index(request):
-#     return HttpResponse("Hello, world. You're at the polls index.")
-def profil(request):
-    return render(request, 'profil.html')
-def beranda(request):
-    return render(request, 'beranda.html')
+def register(request):
+    return render(request,'register.html')
