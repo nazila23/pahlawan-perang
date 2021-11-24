d5a567375525d54a6948a8550e4381c2456634ed
nazila23 <nazila.pudjakesuma@gmail.com>
2021-11-24 09:53:08 +0700

    iki wae

--- main/urls.py
+++ main/urls.py
@@ -4,10 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-<<<<<<< HEAD
     path('register/', views.register, name='register'),
-=======
-    # path('profil/', views.profil, name='profil'),
-    # path('beranda/', views.beranda, name='beranda'),
->>>>>>> 6994170d9d9747d16fe317ddcc1629ebadab35ce
 ]
