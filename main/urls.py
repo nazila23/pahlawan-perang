334fea6d7645142ef0e8a002ebd1db3c64a4140f
Nursidiq97 <sidiqnur363@gmail.com>
2021-11-25 12:21:06 +0700

    terserah

--- main/urls.py
+++ main/urls.py
@@ -5,12 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('register/', views.register, name='register'),
-<<<<<<< HEAD
     path('order/', views.order, name='order'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('tambah/', views.tambah, name='tambah'),
-]
-=======
     path('usersprof/', views.usersprof, name='usersprof'),
 ]
->>>>>>> 08eea0039e7f69205b1c23f4579431d38e3b1503
